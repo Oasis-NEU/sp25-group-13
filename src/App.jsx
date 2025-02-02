d0a550c98546a02231d26161900ad4a5edeabcb3
ColinQuigley <quigley.co@northeastern.edu>
2025-02-02 13:17:50 -0500

    added links for app and home

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from './Pages/About'
+import Account from './Pages/Account View'
+import Calendar from './Pages/Calendar'
+import Discover from './Pages/Discover'
+import Home from './Pages/Home'
 import Login from './Pages/Login'
+import Profile from './Pages/Profile'
+import Search from './Pages/Search'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    <Login/>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home></Home>}></Route>
+        <Route exact path="/about" element={<About></About>}></Route>
+        <Route exact path="/account" element={<Account></Account>}></Route>
+        <Route exact path="/calendar" element={<Calendar></Calendar>}></Route>
+        <Route exact path="/discover" element={<Discover></Discover>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/profile" element={<Profile></Profile>}></Route>
+        <Route exact path="/search" element={<Search></Search>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,23 @@
 import './Home.css'
 import{BrowserRouter as Router, Routes, Route } from 'react-router-d'
+import { Link } from "react-router-dom";
+import About from "./Pages/About"
+import Search from "./Pages/Search"
+import Calendar from "./Pages/Calendar"
+import Discover from "./Pages/Discover"
+import Account from "./Pages/Account"
 
 function Home() {
 
   return (
-    <Router>
-
-    </Router>
+    <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/calendar">Calendar</Link>
+        <Link to="/discover">Discover</Link>
+        <Link to="/account">Account</Link>
+    </div>
   )
 }
 export default Login
