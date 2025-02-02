e7fb6b73da82ec978fba4e79fec8cb93358384f1
jasendo1 <jack.senders@gmail.com>
2025-02-02 13:58:34 -0500

    added functionality for button

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,17 +1,27 @@
 import './Login.css'
+import {useState, useEffect, useRef} from 'react';
+
+
 
 
 function Login() {
-
+  const [activeComponent, setActiveComponent] = useState('');
   return (
     <>
     <div className="login-container">
       <h1 className="login-header">Login</h1>
         <div className="button-container"></div>
-        <button className="login-button">Login as Artist</button>
+        <button onClick={() => setActiveComponent('login-as-artist')}
+        className="login-button">Login as Artist</button>
         <button className="login-button">Login as Listener</button>
+        {activeComponent === "login-as-artist" && (
+          <div className = "artist-login">
+            <h1>test</h1>
+          </div>
+        )}
       </div>
     </>
+
   )
 }
 
