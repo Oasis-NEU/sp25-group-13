c3a77f4704d1d615cf4df735b5caf2381961bc0c
ColinQuigley <quigley.co@northeastern.edu>
2025-02-02 13:54:18 -0500

    Added head

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,29 @@
 import './Home.css'
-import{BrowserRouter as Router, Routes, Route } from 'react-router-d'
+import{BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Link } from "react-router-dom";
-import About from "./Pages/About"
-import Search from "./Pages/Search"
-import Calendar from "./Pages/Calendar"
-import Discover from "./Pages/Discover"
-import Account from "./Pages/Account"
+import About from "../About/About.jsx";
+import Search from "../Search/Search.jsx";
+import Calendar from "../Calendar/Calendar.jsx";
+import Discover from "../Discover/Discover.jsx";
+import Account from "../Account View/Account.jsx";
 
 function Home() {
 
   return (
-    <html>
-        <head>
-            <div class="Links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/search">Search</Link>
-                <Link to="/calendar">Calendar</Link>
-                <Link to="/discover">Discover</Link>
-                <Link to="/account">Account</Link>
-            </div>
-        </head>
-        <body>
-            <h1>Home</h1>
-        </body>
-    </html>
+    <div>
+    <div className="Links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/calendar">Calendar</Link>
+        <Link to="/discover">Discover</Link>
+        <Link to="/account">Account</Link>
+    </div>
+
+    <div className="Graphics">
+        <h1>Home</h1>
+    </div>
+    </div>
   )
 }
-export default Login
+export default Home
