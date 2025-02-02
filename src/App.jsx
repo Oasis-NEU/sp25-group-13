674b8517e368a21c7f1d59ab71076a3d0a5eebcf
snyder2406 <shelbychsnyder@gmail.com>
2025-02-02 13:13:21 -0500

    fixed files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Pages/Login'
+import Login from './Pages/Login/Login.jsx'
 
 function App() {
 
