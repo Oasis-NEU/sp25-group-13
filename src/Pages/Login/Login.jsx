06da6f31709eb5ac07ae37b8eb2c9218d1d2ea27
jasendo1 <jack.senders@gmail.com>
2025-02-09 13:03:03 -0500

    fixed login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,26 @@
 import './Login.css'
-import {useState, useEffect, useRef} from 'react';
-
-
-
+import { Link } from "react-router-dom";
 
 function Login() {
   const [activeComponent, setActiveComponent] = useState('');
   return (
-    <>
+    <div>
+    {/* Navigation Links */}
+    <div className="Links">
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/account">Account</Link>
+        <Link to="/calendar">Calendar</Link>
+        <Link to="/discover">Discover</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/search">Search</Link>
+    </div>
+
+    {/* Page Content */}
+    <div className="Graphics">
+      <h1>Login Page</h1>
+    </div>
     <div className="login-container">
       <h1 className="login-header">Login</h1>
         <div className="button-container"></div>
@@ -28,9 +41,10 @@ function Login() {
           </div>
         )}
       </div>
-    </>
-
+    </div>
   )
 }
+
+      
 
 export default Login
